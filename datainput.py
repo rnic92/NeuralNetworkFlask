b39372340c2ef6181fe7206279b8948a805f6915
rnic92 <rnic92@vt.edu>
2021-05-13 11:28:07 -0400

    fixed app and imports

--- datainput.py
+++ datainput.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 import numpy as np
 import pandas as pd
-from skimage.io import imread
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from keras.models import Sequential
-from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras.models import model_from_json
 from keras.utils import np_utils
 import tensorflow as tf
 import cv2
@@ -24,11 +19,9 @@ def Check(filepath):
 
     test_pred = model.predict(val_x)
     test_pred = np.argmax(test_pred,1)
+    return test_pred
 
-
-
-
-if __name__ == '__main__':
+def trainingmodel():
     train = pd.read_csv("digit-recognizer/train.csv")
     test = pd.read_csv('digit-recognizer/test.csv')
     train_y = train['label']
@@ -58,4 +51,17 @@ if __name__ == '__main__':
                  optimizer='adam',
                  metrics=['accuracy'])
     model.fit(train_x,train_y,batch_size=64,epochs=2,verbose=1)
+
+
+if __name__ == '__main__':
+    train = False
+    if train == True:
+        trainingmodel()
+    else:
+        json_file = open("model1.json", 'r')
+        lmodel = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(lmodel)
+        loaded_model.load_weights("model.h5")
+        loaded_model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
     app.run(port=8081, debug=True)
